8d8a96e43718365a930ff811e0d40ff92cccd372
kfoxb <kfoxb@kfoxb.com>
2017-02-25 13:35:25 -0800

    Link all templates. Pass sample video data to videoList, videoListEntry, and videoPlayer. Correctly render sample data. Not pass final videoPlayer test.

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,15 @@
 angular.module('video-player')
 
+.controller('AppCtrl', function($scope) {
+
+  $scope.videos = window.exampleVideoData;
+  $scope.currentVideo = $scope.videos[0];
+  $scope.onClick = function() {};
+})
+
+
 .directive('app', function() {
   return {
-    // TODO
+    templateUrl: 'src/templates/app.html'
   };
 });
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,21 @@
 angular.module('video-player')
+
 .directive('videoList', function() {
   return {
-    // TODO
+
+    scope: {
+      videos: '<',
+      onClick: '<'
+    },
+
+    controllerAs: 'ctrl',
+
+    bindToController: true,
+
+    controller: function($scope) {
+      console.log($scope);
+    },
+
+    templateUrl: 'src/templates/videoList.html'
   };
 });
